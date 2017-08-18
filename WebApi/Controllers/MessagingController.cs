f7f74f6d212ab9431d6d332a68dd5b47ac709e66
Sarvesh Upadhyay <sarvesh.upadhyay@tavant.com>
2017-08-18 11:17:45 +0530

    Cobrowsing basic ready

---
+++ WebApi/Controllers/MessagingController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Web.WebSockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.WebSockets;
+
+namespace WebApi.Controllers
+{
+    public class MessagingController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            var currentContext = HttpContext.Current;
+            if (currentContext.IsWebSocketRequest ||
+                currentContext.IsWebSocketRequestUpgrading)
+            {
+                currentContext.AcceptWebSocketRequest(ProcessWebsocketSession);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.SwitchingProtocols);
+        }
+
+        private async Task ProcessWebsocketSession(AspNetWebSocketContext context)
+        {
+            var handler = new MyWebSocketHandler();
+             await handler.ProcessWebSocketRequestAsync(context);
+            //return processTask;
+        }
+
+        //public HttpResponseMessage Post(string message)
+        //{
+        //    var currentContext = HttpContext.Current;
+        //    if (currentContext.IsWebSocketRequest ||
+        //        currentContext.IsWebSocketRequestUpgrading)
+        //    {
+        //        currentContext.AcceptWebSocketRequest(ProcessWebsocketSession);
+        //    }
+
+        //    return Request.CreateResponse(HttpStatusCode.SwitchingProtocols);
+        //}
+
+        public HttpResponseMessage Post(byte[] message)
+        {
+            var currentContext = HttpContext.Current;
+            if (currentContext.IsWebSocketRequest ||
+                currentContext.IsWebSocketRequestUpgrading)
+            {
+                currentContext.AcceptWebSocketRequest(ProcessWebsocketSession);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.SwitchingProtocols);
+        }
+    }
+}
---
+++ WebApi/MyWebSocketHandler.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Web.WebSockets;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+using WebApi;
+
+namespace WebApi
+{
+    public class MyWebSocketHandler: WebSocketHandler
+    {
+        private static WebSocketCollection clients = new WebSocketCollection();
+
+        public override void OnOpen()
+        {
+           
+            clients.Add(this);
+          //  base.Send("You are connected to a WebSocket your turn: "+clients.Count);
+
+        }
+
+        public override void OnMessage(string message)
+        {
+            Debugger.Log(1, "info", message);
+            foreach (var client in clients)
+            {
+                client.Send(message);
+            }
+        }
+        public override void OnMessage(byte[] message)
+        {
+          //  Debugger.Log(1, "info", message);
+            foreach (var client in clients)
+            {
+                client.Send(message);
+            }
+        }
+        public override void OnClose()
+        {
+            Close();
+        }
+    }
+}
