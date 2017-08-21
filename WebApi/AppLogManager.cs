afef7c9662e572e59847e1d523e3be0e3bf12b90
sarvesh.upadhyay <sarvesh189@gmail.com>
2017-08-21 18:14:52 +0530

    log added

---
+++ WebApi/AppLogManager.cs
@@ -0,0 +1,53 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebApi
+{
+    public class AppLogManager
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public static void LogInfo(string message)
+        {
+            logger.Info(message);
+        }
+
+        public static void LogError(Exception ex)
+        {
+            logger.Error(ex);
+        }
+
+        public static string LogFolder()
+        {
+            var target = LogManager.Configuration.FindTargetByName("default") as NLog.Targets.FileTarget;
+            var logfileName = target.FileName.ToString().Trim('\'');
+            return logfileName.Substring(0, logfileName.LastIndexOf('\\'));
+        }
+
+        public static async Task<string> ReadLog(string file)
+        {
+            string readTextTask = null;
+
+            if (File.Exists(file))
+            {
+                using (var reader = new StreamReader(file))
+                {
+                    readTextTask = await reader.ReadToEndAsync();
+
+                }
+            }
+
+            return readTextTask;
+        }
+    }
+    public enum AppLogLevel
+    {
+        Info = 0,
+        Error = 1
+    }
+}
--- WebApi/MyWebSocketHandler.cs
+++ WebApi/MyWebSocketHandler.cs
@@ -10,94 +10,92 @@ using WebApi;
 
 namespace WebApi
 {
-    public class MyWebSocketHandler: WebSocketHandler
+    public class MyWebSocketHandler : WebSocketHandler
     {
         private static WebSocketCollection clients = new WebSocketCollection();
 
         private static Dictionary<Guid, string> messages = new Dictionary<Guid, string>();
-       private Random random = new Random();
+        private Random random = new Random();
         JavaScriptSerializer serializer = new JavaScriptSerializer();
         public CobrowsingUser User { get; set; } = new CobrowsingUser();
 
         public Guid ChatroomId { get; set; }
         public override void OnOpen()
         {
-            SocketMessage socketMessage = null;
-            var chatid = this.WebSocketContext.QueryString["chatroomid"];
-            if (string.IsNullOrEmpty(chatid) || chatid == "undefined")
-                return;
-            this.ChatroomId = Guid.Parse(chatid);
-                   
-            
-
-            
-            var id = random.Next(1, 10000).ToString();
-            var message = new SocketMessage() { MessageType = "1", UserId = id, ChatRoomId = chatid, Message = "connected", UserCount = "0", ScrollX = "0", ScrollY = "0", X = "0", Y = "0" };
-            Send(serializer.Serialize(message));
-            this.User.Id = id;
-            clients.Add(this);
-            var groupclients = clients.Where(cl => ((MyWebSocketHandler)cl).ChatroomId.Equals(ChatroomId));
-           
-            string lastmessage;
-            if (messages.TryGetValue(this.ChatroomId, out lastmessage))
+            try
             {
-                socketMessage = serializer.Deserialize<SocketMessage>(lastmessage);
-                socketMessage.UserCount = groupclients.Count().ToString();
-                socketMessage.MessageType = "3";
-            ////    foreach (var client in groupclients)
-            //    {
-                    Send(serializer.Serialize(socketMessage));
-              //  }
-            }
-           
+                SocketMessage socketMessage = null;
+                var chatid = this.WebSocketContext.QueryString["chatroomid"];
+                if (string.IsNullOrEmpty(chatid) || chatid == "undefined")
+                    return;
+                this.ChatroomId = Guid.Parse(chatid);
 
-           
-        //    base.Send();
+                var id = random.Next(1, 10000).ToString();
+                var message = new SocketMessage() { MessageType = "1", UserId = id, ChatRoomId = chatid, Message = "connected", UserCount = "0", ScrollX = "0", ScrollY = "0", X = "0", Y = "0" };
+                Send(serializer.Serialize(message));
+                this.User.Id = id;
+                clients.Add(this);
+                var groupclients = clients.Where(cl => ((MyWebSocketHandler)cl).ChatroomId.Equals(ChatroomId));
+
+                string lastmessage;
+                if (messages.TryGetValue(this.ChatroomId, out lastmessage))
+                {
+                    socketMessage = serializer.Deserialize<SocketMessage>(lastmessage);
+                    socketMessage.UserCount = groupclients.Count().ToString();
+                    socketMessage.MessageType = "3";
+                    Send(serializer.Serialize(socketMessage));
+
+                }
+
+                AppLogManager.LogInfo($"new user connected to {chatid} and is assigned the userid: {this.User.Id}");
+
+            }
+            catch (Exception ex)
+            {
+                AppLogManager.LogError(ex);
+            }
+            //    base.Send();
 
         }
 
         public override void OnMessage(string message)
         {
-             var chatroomid = this.WebSocketContext.QueryString["chatroomid"];
-            var messageObject = (SocketMessage)serializer.Deserialize(message, typeof(SocketMessage));
-            if (string.IsNullOrEmpty(chatroomid) || chatroomid == "undefined")
+            try
             {
-                return;
-                //var client = clients.FirstOrDefault(cl => cl == );
-                //var socketMessage = new SocketMessage()
-                //{
-                //    ChatRoomId = "0000",
-                //    Message = "chat room not available",
-                //    MessageId = Guid.NewGuid().ToString(),
-                //    MessageType = "2", //" 2: observer connected"
-                //    UserId = "NA"
-                //};
-                //if(client != null)
-                //client.Send((new JavaScriptSerializer()).Serialize(socketMessage));
-            }
-            else
-            {
-                if (messageObject.MessageType == "3")
+                var chatroomid = this.WebSocketContext.QueryString["chatroomid"];
+                AppLogManager.LogInfo($"chatroomid: {chatroomid} message:{message}");
+                var messageObject = (SocketMessage)serializer.Deserialize(message, typeof(SocketMessage));
+                if (string.IsNullOrEmpty(chatroomid) || chatroomid == "undefined")
                 {
-                    if (messages.ContainsKey(Guid.Parse(chatroomid)))
+                    AppLogManager.LogInfo("chatroomid is not available");
+                    return;
+                }
+                else
+                {
+                    if (messageObject.MessageType == "3")
                     {
-                        messages[Guid.Parse(chatroomid)] = message;
+                        if (messages.ContainsKey(Guid.Parse(chatroomid)))
+                        {
+                            messages[Guid.Parse(chatroomid)] = message;
+                        }
+                        else
+                        {
+                            messages.Add(Guid.Parse(chatroomid), message);
+                        }
                     }
-                    else
+                    var groupclients = clients.Where(cl => ((MyWebSocketHandler)cl).ChatroomId.Equals(Guid.Parse(chatroomid)) && ((MyWebSocketHandler)cl).User.Id != messageObject.UserId);
+                    foreach (var client in groupclients)
                     {
-                        messages.Add(Guid.Parse(chatroomid), message);
+                        client.Send(message);
+                        AppLogManager.LogInfo($"message published to {(client as MyWebSocketHandler).User.Id}");
                     }
                 }
-                var groupclients = clients.Where(cl => ((MyWebSocketHandler)cl).ChatroomId.Equals(Guid.Parse(chatroomid)) && ((MyWebSocketHandler)cl).User.Id != messageObject.UserId );
-                foreach (var client in groupclients)
-                {
-                    client.Send(message);
-                }
             }
-            //foreach (var client in clients)
-            //{
-            //    client.Send(message);
-            //}
+            catch (Exception ex)
+            {
+                AppLogManager.LogError(ex);
+            }
+
         }
         public override void OnMessage(byte[] message)
         {
@@ -110,7 +108,15 @@ namespace WebApi
         }
         public override void OnClose()
         {
-            Close();
+            try
+            {
+                Close();
+                AppLogManager.LogInfo("Connection closed!");
+            }
+            catch (Exception ex)
+            {
+                AppLogManager.LogError(ex);
+            }
         }
     }
 }
